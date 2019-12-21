a1beab1ed0513553b5705442af8b04d4115bec33
rth970117 <994436856@qq.com>
2019-12-21 20:51:41 +0800

    springboot learning

--- src/main/java/com/rth/bootdemo/BootdemoApplication.java
+++ src/main/java/com/rth/bootdemo/BootdemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootdemoApplication {
-
+    int a;
     public static void main(String[] args) {
         SpringApplication.run(BootdemoApplication.class, args);
     }
