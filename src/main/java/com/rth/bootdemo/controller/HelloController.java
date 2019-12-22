1d02e6e9efc5fe2233ff811129760bf1d86b0160
rth970117 <994436856@qq.com>
2019-12-22 21:48:27 +0800

    test lombok

--- src/main/java/com/rth/bootdemo/controller/HelloController.java
+++ src/main/java/com/rth/bootdemo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.rth.bootdemo.controller;
 
 
+import com.rth.bootdemo.model.Article;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
+
     @RequestMapping("/hello")
     public String hello() {
+
+        Article article = new Article(1L,"Tom");
+        article.setAuthor("tom");
+
         return "hello world";
     }
 
