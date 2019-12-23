b2c84ba5ffcb4fc2a5970559f33b3bc1c388cdc7
rth <502175457@qq.com>
2019-12-23 10:43:55 +0800

    test lombox

--- src/main/java/com/rth/bootdemo/controller/HelloController.java
+++ src/main/java/com/rth/bootdemo/controller/HelloController.java
@@ -2,21 +2,26 @@ package com.rth.bootdemo.controller;
 
 
 import com.rth.bootdemo.model.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
 
 
     @RequestMapping("/hello")
-    public String hello() {
+    public Article hello() {
 
         Article article = new Article(1L,"Tom");
         article.setAuthor("tom");
 
-        return "hello world";
+        Article article1 = Article.builder().id(2L).author("Bob").build();
+
+        log.info("测试一下"+article1);
+        return article1;
     }
 
 }
--- src/main/java/com/rth/bootdemo/model/Article.java
+++ src/main/java/com/rth/bootdemo/model/Article.java
@@ -1,12 +1,14 @@
 package com.rth.bootdemo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Builder
 public class Article {
     private Long id;
     private String author;
