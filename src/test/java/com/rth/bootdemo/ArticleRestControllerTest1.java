d63f80413d4b75e17ddfa0ba2f2f152167d226e6
rth <502175457@qq.com>
2019-12-24 17:47:16 +0800

    test RunWith(SpringRunner.class)

---
+++ src/test/java/com/rth/bootdemo/ArticleRestControllerTest1.java
@@ -0,0 +1,63 @@
+package com.rth.bootdemo;
+
+import com.rth.bootdemo.controller.ArticleRestController;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+//@Transactional
+@Slf4j
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ArticleRestControllerTest1 {
+
+    //mock对象
+    @Resource
+    private MockMvc mockMvc;
+
+    //mock对象初始化
+   /* @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleRestController()).build();
+    }*/
+
+    //测试方法
+    @Test
+    public void saveArticle() throws Exception {
+
+        String article = "{\n" +
+                "    \"id\": 1,\n" +
+                "    \"author\": \"zimug\",\n" +
+                "    \"title\": \"手摸手教你开发spring boot\",\n" +
+                "    \"content\": \"c\",\n" +
+                "    \"createTime\": \"2017-07-16 05:23:34\",\n" +
+                "    \"reader\":[{\"name\":\"zimug\",\"age\":18},{\"name\":\"kobe\",\"age\":37}]\n" +
+                "}";
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.request(HttpMethod.POST, "/rest/article")
+                .contentType("application/json").content(article))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.author").value("zimug"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.reader[1].age").value(37))
+                .andDo(print())
+                .andReturn();
+
+        log.info(result.getResponse().getContentAsString());
+
+    }
+}
